e5fdf8ee07c6012ffe1e1190360cdf041f8aa6e3
Joel <joel.balmer@gmail.com>
2018-03-03 22:08:50 +0000

    - Added in error logging middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let express = require('express');
 let app = express();
 let server = app.listen(3000);
 
+// show public site
 app.use(express.static("public"));
 
 // request logging
@@ -11,9 +12,19 @@ const requestLogging = (req, res, next) => {
 	console.log(`A ${req.method} request was just made`);
 	next();
 }
-
 app.use(requestLogging);
 
 // handle the CRUD for beers
 const beersRouter = require('./routers/beers-router.js');
 app.use('/beers/', beersRouter);
+
+// error handling
+const errorHandler = (err, req, res, next) => {
+  if (!err.status) {
+    err.status = 500;
+  }
+  
+  res.status(err.status).send(err.message);
+  next(err);
+};
+app.use(errorHandler);
