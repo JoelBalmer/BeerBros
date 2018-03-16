5046be602d1488eb6da7af532ad1c77ecb75b9d4
Joel <joel.balmer@gmail.com>
2018-03-16 17:33:06 +0000

    added a passport file to handle request, bit out my deptch at the moment

---
+++ config/auth.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '444071202692690', // your App ID
+        'clientSecret'  : '4c63987c593c7fd4db3fc11590bdba00', // your App Secret
+        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback',
+        'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
+        'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
